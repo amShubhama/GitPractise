161f6448138bd1c1b070bb00b59d324c89ac59df
amShubhama <shubham14sk32@gmail.com>
2025-01-05 14:46:40 +0530

    Modified after cloining

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
 class Test{
+public static int sum(int a,int b){
+      return a+b;
+}
 public static void main(String[] args){
     System.out.println("Exploring Git & GitHub");
     System.out.println("Exploring some commands");
+    Sytem.out.println("Sum is: "+sum(5+2);
 }
 }
